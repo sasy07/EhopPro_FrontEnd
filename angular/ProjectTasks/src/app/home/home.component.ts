91f4f833f4918c7698c7b3379b93e748643ddd52
MrLost\PC <MrLost@Yahoo.com>
2020-11-27 19:01:37 +0330

    First Commit (E04)

---
+++ angular/ProjectTasks/src/app/home/home.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent implements OnInit {
+  itemCounter = 0;
+  projectText = 'Angular Project';
+  projects: string[] = [];
+  constructor() {}
+
+  ngOnInit(): void {
+    this.itemCounter = this.projects.length;
+  }
+
+  // tslint:disable-next-line: typedef
+  addItem() {
+    this.projects.push(this.projectText);
+    this.projectText = '';
+    this.itemCounter = this.projects.length;
+  }
+
+  // tslint:disable-next-line: typedef
+  removeItem(i: number) {
+    this.projects.splice(i, 1);
+    this.itemCounter = this.projects.length;
+  }
+}
